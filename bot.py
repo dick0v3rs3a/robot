adfa47968c0c58fdbdaa003ee7a13650a235d011
dick0v3rs3a <35269972+dick0v3rs3a@users.noreply.github.com>
2018-07-04 14:12:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,13 @@ lass WebhookServer(object):
 def echo_message(message):
     bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=["text"])
+def default_test(message):
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_mark
+
 bot.remove_webhook()
 
  # Ставим заново вебхук
